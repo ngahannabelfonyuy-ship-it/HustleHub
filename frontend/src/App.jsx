82ff93ef14ae2fbeac411d80369abee8d275f7be
Asungnyu <=>
2026-06-04 00:38:04 +0100

    frontend created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+
+import {Routes, BrowserRouter, Route} from 'react-router-dom'
+import LandingPage from './pages/LandingPage'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/HeroSection.jsx
@@ -0,0 +1,11 @@
+export default function HeroSection(){
+    return (
+      <>
+        <p className='text-center text-[18pt] '>Welcome to HustleHub</p>
+        <div className="w-full flex justify-center items-center gap-x-4">
+          <a href="#">create a job</a>
+          <a href="#">find a job</a>
+        </div>
+      </>
+    );
+};
